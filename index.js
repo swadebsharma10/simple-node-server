e45db9fd3ce6ff4f5e93540f425699b51b862e31
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-30 04:56:47 +0600

    explore how to connect mongodn and discuss post and get api

--- index.js
+++ index.js
@@ -9,40 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const users = [
-    {
-      "id": 1,
-      "name": "John Doe",
-      "email": "john@example.com"
-    },
-    {
-      "id": 2,
-      "name": "Jane Smith",
-      "email": "jane@example.com"
-    },
-    {
-      "id": 3,
-      "name": "Bob Johnson",
-      "email": "bob@example.com"
-    },
-    {
-      "id": 4,
-      "name": "Alice Brown",
-      "email": "alice@example.com"
-    },
-    {
-      "id": 5,
-      "name": "Charlie Davis",
-      "email": "charlie@example.com"
-    },
-    {
-      "id": 6,
-      "name": "Eva White",
-      "email": "eva@example.com"
-    },
-]
-
-
 
 
 const uri = `mongodb+srv://Node24DB:W69Qwo3DlZ0gczuB@cluster0.fikwith.mongodb.net/?retryWrites=true&w=majority`;
@@ -60,6 +26,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const usersCollection = client.db("UsersDB24").collection("users");
+
+    app.post('/users', async(req, res) =>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result)
+    })
+
+
+
+    app.get('/users', async(req, res) =>{
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -72,20 +58,8 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('simple node server is running')
-})
-
-app.get('/users', (req, res) =>{
-    res.send(users);
 });
-
-app.post('/users', (req, res) =>{
-    const user = req.body;
-    user.id = users.length + 1;
-    users.push(user);
-    res.send(user)
-    console.log(user)
-})
 
 app.listen(port, () => {
   console.log(`simple node server is running on port ${port}`)
-})
+});
