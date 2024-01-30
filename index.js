f17872693084577acb0aeddf4adc9b38943d31bd
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-30 20:29:14 +0600

    final deploy

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
 
     const usersCollection = client.db("UsersDB24").collection("users");
 
@@ -46,9 +45,7 @@ async function run() {
 
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    
   } finally {
     
   }
